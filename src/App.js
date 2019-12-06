3db90218a41f3951a3a0e6d7802665276517cd12
Paul Perkins <pperkins@apple.com>
2019-12-06 17:43:17 -0600

    add event handling/listening, currency logic

--- src/App.js
+++ src/App.js
@@ -10,26 +10,60 @@ class App extends Component {
 		super(props);
 		this.state = {
       dollar: 0,
+      selectedOption: '',
       currencySign: '',
 		}
   }
 
- 
-  getData = (event) => {
-    event.preventDefault();
+  getData = (e) => {
+    e.preventDefault();
     axios({
       url: url,
-      method: 'get'
+      method: 'GET'
     })
     .then((res) => {
-      let currencyAmount = res.data.bpi.USD.rate_float;
-      let money = currencyAmount.toFixed(2);
-      this.setState({dollar: money, currencySign: "$"})
+      if (this.state.selectedOption === 'usd'){
+        let symbol = res.data.bpi.USD.symbol;
+        let currencyAmount = res.data.bpi.USD.rate_float;
+        let money = currencyAmount.toFixed(2);
+        this.setState({
+          dollar: money,
+          currencySign: symbol
+        })
+      }else if (this.state.selectedOption === 'gbp') {
+          let symbol = res.data.bpi.GBP.symbol;
+          let currencyAmount = res.data.bpi.GBP.rate_float;
+          let money = currencyAmount.toFixed(2);
+          this.setState({
+            dollar: money,
+            currencySign: symbol
+          })
+      } else if (this.state.selectedOption === 'eur') {
+          let symbol = res.data.bpi.EUR.symbol;
+          let currencyAmount = res.data.bpi.EUR.rate_float;
+          let money = currencyAmount.toFixed(2);
+          this.setState({
+            dollar: money,
+            currencySign: symbol
+          })
+      } else {
+        this.setState({
+          dollar: " ",
+          currencySign: " ",
+        })
+      }
     })
     .catch(function (error) {
       console.log(error);
     });
   }
+
+  handleOptionChange = (e) => {
+    this.setState({
+      selectedOption: e.currentTarget.value
+      });
+  }
+
   render() {
   return (
     <div>
@@ -45,21 +79,43 @@ class App extends Component {
     <div className="col-12 d-flex justify-content-center">
       <form>
           <div className="form-check form-check-inline text-center">
-              <input className="form-check-input" type="radio" name="currency" id="inlineRadio1" value="usd" checked />
-              <label className="form-check-label" for="inlineRadio1">USD</label>
+              <input 
+                className="form-check-input" 
+                type="radio" 
+                name="currency" 
+                id="inlineRadio1" 
+                value="usd" 
+                checked={this.state.selectedOption === 'usd'}
+                onChange={this.handleOptionChange}
+              />
+              <label className="form-check-label" htmlFor="inlineRadio1">USD</label>
           </div>
           <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="currency" id="inlineRadio2" value="gbp" />
-              <label className="form-check-label" for="inlineRadio2">GBP</label>
+              <input 
+                className="form-check-input" 
+                type="radio" name="currency" 
+                id="inlineRadio2" 
+                value="gbp" 
+                checked={this.state.selectedOption === 'gbp'} 
+                onChange={this.handleOptionChange}
+              />
+              <label className="form-check-label" htmlFor="inlineRadio2">GBP</label>
           </div>
           <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="currency" id="inlineRadio3" value="eur" />
-              <label className="form-check-label" for="inlineRadio3">EUR</label>
+              <input 
+                className="form-check-input" 
+                type="radio" name="currency" 
+                id="inlineRadio3" 
+                value="eur"
+                checked={this.state.selectedOption === 'eur'} 
+                onChange={this.handleOptionChange}
+              />
+              <label className="form-check-label" htmlFor="inlineRadio3">EUR</label>
           </div>
           <div className="d-flex justify-content-center mb-5">
               <input onClick={this.getData} className="mt-4 btn btn-primary" type="submit" value="Get Price!" />
           </div>
-          {this.state.currencySign}{this.state.dollar}
+          {this.state.currencySign}{this.state.dollar}{this.state.selectedOption}
       </form>
     </div>
     </div>
