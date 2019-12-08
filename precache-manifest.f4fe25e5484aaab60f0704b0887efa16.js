1b037ce10921174fc4b843f3ae9816d016fa7f50
Paul Perkins <paulperkins7@gmail.com>
2019-12-07 18:32:57 -0600

    Updates

---
+++ precache-manifest.f4fe25e5484aaab60f0704b0887efa16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd0297a091588996db869e4420085fe",
+    "url": "/bitCoin/index.html"
+  },
+  {
+    "revision": "a5e1d87bbee6f40a7f22",
+    "url": "/bitCoin/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "22cf1c9873578f787c69",
+    "url": "/bitCoin/static/css/main.28e24ca4.chunk.css"
+  },
+  {
+    "revision": "a5e1d87bbee6f40a7f22",
+    "url": "/bitCoin/static/js/2.0da4abbf.chunk.js"
+  },
+  {
+    "revision": "22cf1c9873578f787c69",
+    "url": "/bitCoin/static/js/main.bac1a184.chunk.js"
+  },
+  {
+    "revision": "2cf9bd4f48e371f97d51",
+    "url": "/bitCoin/static/js/runtime-main.29b26cc9.js"
+  }
+]);
